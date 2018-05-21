fef6bcfcb360f3619244c6ee5635cd2927c7fadd
parsaaes <parsaaes@gmail.com>
2018-05-22 01:21:31 +0430

    Complete removing of useless products

--- CF-Grammar-Simplifier/src/ir/ac/aut/ceit/tml/GrammarTools/CFSimplifier.java
+++ CF-Grammar-Simplifier/src/ir/ac/aut/ceit/tml/GrammarTools/CFSimplifier.java
@@ -2,11 +2,33 @@ package ir.ac.aut.ceit.tml.GrammarTools;
 
 import ir.ac.aut.ceit.tml.Grammar.Grammar;
 import ir.ac.aut.ceit.tml.Grammar.Production;
+import ir.ac.aut.ceit.tml.Graph.Graph;
 
 import java.util.ArrayList;
 
 public class CFSimplifier {
-    public void removeUselessProducts(Grammar grammar){
+    public void removeUselessProducts(Grammar grammar) {
+        removeUnableToGiveTerminal(grammar);
+        removeUnableToGetFromStart(grammar);
+    }
+
+    public void removeUnableToGetFromStart(Grammar grammar) {
+        Graph varGraph = new Graph(grammar.getVars());
+        for (Production production : grammar.getProductions()) {
+            char left = production.getLeftSide();
+            for (char right : production.getRightSide().toCharArray()) {
+                if(grammar.getVars().contains(String.valueOf(right))){
+                    varGraph.insertEdge(left, right);
+                }
+            }
+        }
+        varGraph.runDFS(grammar.getStartVar());
+        grammar.setVars(varGraph.getVisitedNodes());
+        setNewProducts(grammar,varGraph.getVisitedNodes());
+        // System.out.println(varGraph.getVisitedNodes());
+    }
+
+    private void removeUnableToGiveTerminal(Grammar grammar){
         String v1 = "";
         // v1 += grammar.getStartVar();
         boolean added = true;
@@ -14,22 +36,34 @@ public class CFSimplifier {
             added = false;
             for (char v : grammar.getVarsAsArr()) {
                 for (Production production : grammar.getProductions()) {
-                    if(production.getLeftSide() == v && rightSideIsInV1T(production.getRightSide(), v1, grammar.getTerminals()) && !v1.contains(String.valueOf(v))){
+                    if(production.getLeftSide() == v &&
+                            rightSideIsInV1T(production.getRightSide(), v1, grammar.getTerminals()) && !v1.contains(String.valueOf(v))){
+                        v1 += v;
+                        added = true;
+                    }
+                    // for taking care of start -> lambda(^)
+                    else if(
+                    (v == grammar.getStartVar()  && production.getLeftSide() == grammar.getStartVar() && production.getRightSide().equals("^"))
+                            && !v1.contains(String.valueOf(v))){
                         v1 += v;
                         added = true;
                     }
                 }
             }
         }
-        System.out.println("out");
+        grammar.setVars(v1);
+        setNewProducts(grammar, v1);
+    }
 
+    private void setNewProducts(Grammar grammar, String newVars) {
         ArrayList<Production> newProducts = new ArrayList<>();
         for (Production production : grammar.getProductions()) {
             boolean productionIsSafe = true;
-            if(v1.contains(String.valueOf(production.getLeftSide()))){
+            if(newVars.contains(String.valueOf(production.getLeftSide()))){
                 for (char c : production.getRightSide().replaceAll("|", "").toCharArray()) {
-                    if(!(v1.contains(String.valueOf(c)) || grammar.getTerminals().contains(String.valueOf(c)))){
-                       productionIsSafe = false;
+                    // taking care of start -> lambda(^)
+                    if(!(newVars.contains(String.valueOf(c)) || grammar.getTerminals().contains(String.valueOf(c)) || c == '^')){
+                        productionIsSafe = false;
                     }
                 }
             }
@@ -52,4 +86,5 @@ public class CFSimplifier {
         }
         return true;
     }
+
 }
--- CF-Grammar-Simplifier/src/ir/ac/aut/ceit/tml/Main.java
+++ CF-Grammar-Simplifier/src/ir/ac/aut/ceit/tml/Main.java
@@ -2,14 +2,16 @@ package ir.ac.aut.ceit.tml;
 
 import ir.ac.aut.ceit.tml.GrammarTools.CFSimplifier;
 import ir.ac.aut.ceit.tml.GrammarTools.Parser;
+import ir.ac.aut.ceit.tml.Graph.Graph;
 
 public class Main {
 
     public static void main(String[] args) {
-        Parser parser = new Parser("test1.txt");
+        Parser parser = new Parser("test.txt");
         CFSimplifier cfSimplifier = new CFSimplifier();
         cfSimplifier.removeUselessProducts(parser.getGrammar());
-        System.out.println(parser.getGrammar());
+         System.out.println(parser.getGrammar());
+      //  cfSimplifier.removeUnableToGetFromStart(parser.getGrammar());
 
     }
 }
